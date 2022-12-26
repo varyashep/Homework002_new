bb1c4a04a8aeee1178545e9438a71090b35929fc
Barbara <varya.sheptunova@gmail.com>
2022-12-26 19:47:54 +0300

    Task 1 Finding second digit

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+WriteLine("Введите трёхзначное число:");
+int number = int.Parse(ReadLine()!);
+
+int second_digit = (number / 10) % 10;
+
+Write("Вторая цифра числа: ");
+WriteLine(second_digit);
