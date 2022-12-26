77b6c6911bb4f763086741cbcd0023d652aa9a19
Barbara <varya.sheptunova@gmail.com>
2022-12-26 21:58:39 +0300

    Task 3 Weekend or not

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console; 
+
+WriteLine("Введите цифру, обозначающую день недели:");
+
+int number = int.Parse(ReadLine()!);
+
+if (number > 7)
+{
+    WriteLine("Такого дня недели не существует.");
+}
+else if (number == 6 || number == 7)
+{
+    WriteLine("Да, это выходной");
+}
+else
+{
+    WriteLine("Нет, это не выходной");
+}
