029071cdadd1820845b5417e130fe762c150a789
Barbara <varya.sheptunova@gmail.com>
2022-12-26 19:56:09 +0300

    Task 2 Finding third digit in any number

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+WriteLine("Введите число:");
+ 
+int number = int.Parse(ReadLine()!);
+int third_digit;
+if (number < 100)
+{
+    WriteLine("Третьей цифры нет.");
+}
+else
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+third_digit = number % 10;
+Write("Третья цифра числа: ");
+WriteLine(third_digit);
+}
